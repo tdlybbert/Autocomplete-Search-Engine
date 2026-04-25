5a40537373ca47766c95a753439c057541ca7c8b
tdlybbert <tdennylybbert@gmail.com>
2026-04-24 21:43:52 -0400

    Create test_autocomplete.py

---
+++ test_autocomplete.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+from trie import Trie
+from trie_node import TrieNode
+from loader import Loader
+from autocomplete import Autocomplete
+
