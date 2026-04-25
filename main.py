c2c5873cd1ab4397fff6b3c6d1a10dbef7f371a6
tdlybbert <tdennylybbert@gmail.com>
2026-04-24 22:20:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from autocomplete import Autocomplete
 def main():
   filepath = sys.argv[1] if len(sys.argv) > 1 else "words.txt"
   try:
-    engine = Autocomplete(filename)
+    engine = Autocomplete(filepath)
   except:
     print(f"Could not find word list file '{filepath}'.")
     print("Usage: python main.py [path/to/words.txt]")
